9546d328ff451f0641e7940a888c0ba06f33fab6
dongasai <1514582970@qq.com>
2025-04-19 00:00:48 +0800

    18

--- src/DesignConfig/types/BaseStats.ts
+++ src/DesignConfig/types/BaseStats.ts
@@ -1,4 +1,3 @@
-
 /**
  * 初始战斗属性接口
  * 描述所有战斗单位共有的基础属性
@@ -6,10 +5,16 @@
 interface BaseStats {
   /** 生命值(HP) - 决定单位的生存能力 */
   hp: number;
+  /** 魔法值(MP) - 决定技能释放能力 */  
+  mp: number;
   /** 攻击力(ATK) - 影响普通攻击和技能伤害 */
   attack: number;
-  /** 防御力(DEF) - 减少受到的伤害 */
+  /** 防御力(DEF) - 减少受到的物理伤害 */
   defense: number;
+  /** 魔法攻击力(MATK) - 影响魔法技能伤害 */
+  magicAttack: number;
+  /** 魔法防御力(MDEF) - 减少受到的魔法伤害 */  
+  magicDefense: number;
   /** 速度(SPD) - 影响行动顺序和闪避率 */
   speed: number;
 }
